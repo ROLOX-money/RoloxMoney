d4d978b4bfb1fe481e711a37bbb4a943ff003b1c
Karthik M <karthik.m@infognana.com>
2023-05-31 08:19:54 +0530

    Added supabase library

--- lib/screen/splash_screen/splash_controller.dart
+++ lib/screen/splash_screen/splash_controller.dart
@@ -1,18 +1,50 @@
 import 'dart:async';
+import 'package:flutter/cupertino.dart';
 import 'package:get/get.dart';
+import 'package:roloxmoney/screen/dashboard_screen/dashboard_screen.dart';
+import 'package:roloxmoney/screen/login_profile_screen/login_profile_screen.dart';
 import 'package:roloxmoney/screen/login_screen/login_screen.dart';
+import 'package:roloxmoney/singleton.dart';
 import 'package:roloxmoney/utils/RoloxKey.dart';
+import 'package:roloxmoney/utils/supa_base_control.dart';
+import 'package:supabase_flutter/supabase_flutter.dart';
 /*Chinnadurai Viswanathan*/
 
 class SplashScreenController extends GetxController with StateMixin {
   @override
   void onInit() async {
-    RoloxKey.baseUrl = 'http://65.2.137.244:8080/';
-
     change(null, status: RxStatus.success());
-    Future.delayed(const Duration(seconds: 1), () {
-      Get.offAndToNamed(LoginScreen.routeName);
-    });
+    initializeSupaBase();
     super.onInit();
+  }
+
+  Future<void> initializeSupaBase() async {
+    try {
+      await Supabase.initialize(
+        url: RoloxKey.supaBaseClientEnvUrl,
+        anonKey: RoloxKey.supaBaseClientClientKey,
+      ).then((initializeValue) {
+        Singleton.supabaseInstance = initializeValue;
+        if (initializeValue.client.auth.currentSession != null &&
+            initializeValue.client.auth.currentUser != null) {
+          SupaBaseController.toGetTheSelectedUser(
+                  mobileNumber: initializeValue.client.auth.currentUser?.phone)
+              .then((value) {
+            // if (value is List && value.length == 0) {
+            //   Get.offAndToNamed(LoginProfileScreen.routeName,
+            //       arguments: initializeValue.client.auth.currentUser?.phone);
+            // } else {
+            //   Get.offAndToNamed(DashboardScreen.routeName);
+            // }
+            Get.offAndToNamed(LoginProfileScreen.routeName,
+                arguments: initializeValue.client.auth.currentUser?.phone);
+          });
+        } else {
+          Get.offAndToNamed(LoginScreen.routeName);
+        }
+      });
+    } catch (e) {
+      initializeSupaBase();
+    }
   }
 }
--- lib/singleton.dart
+++ lib/singleton.dart
@@ -1,9 +1,14 @@
 /*Chinnadurai Viswanathan*/
+import 'package:altogic/altogic.dart';
+import 'package:supabase_flutter/supabase_flutter.dart';
+
 class Singleton {
   //1 = real device, 2= Testing device
   int deviceType = 1;
 
   static final Singleton _singleton = Singleton._internal();
+
+  static late final Supabase supabaseInstance;
 
   factory Singleton() {
     return _singleton;
