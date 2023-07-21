c6091be81b6c62b0518cd70aa650799651ec26ab
Karthik M <karthik.m@infognana.com>
2023-07-21 14:32:49 +0530

    create and view invoice has been done

--- lib/screen/invoice_screen/invoice_controller.dart
+++ lib/screen/invoice_screen/invoice_controller.dart
@@ -1,19 +1,14 @@
-import 'dart:async';
 import 'package:flutter/cupertino.dart';
 import 'package:get/get.dart';
-import 'package:intl/intl.dart';
-import 'package:roloxmoney/model/project_model.dart';
-import 'package:roloxmoney/screen/clients_screen/entites/clinet_model.dart';
 import 'package:roloxmoney/screen/invoice_screen/add_invoice/add_invoice_controller.dart';
 import 'package:roloxmoney/screen/invoice_screen/add_invoice/add_invoice_screen.dart';
-import 'package:roloxmoney/screen/projects_screen/add_project/add_project_controller.dart';
-import 'package:roloxmoney/screen/projects_screen/add_project/add_project_screen.dart';
+import 'package:roloxmoney/screen/invoice_screen/entities/invoice_model.dart';
 import 'package:roloxmoney/singleton.dart';
 import 'package:roloxmoney/utils/RoloxKey.dart';
 
 /*Chinnadurai Viswanathan*/
 class InvoiceController extends GetxController with StateMixin {
-  RxList projectInvoicesList = [].obs;
+  RxList invoicesList = [].obs;
 
   @override
   void onInit() async {
@@ -24,8 +19,7 @@ class InvoiceController extends GetxController with StateMixin {
 
   toGetTheInvoiceList() async {
     change(null, status: RxStatus.loading());
-    projectInvoicesList.clear();
-    List<ClientModel> clientList = [];
+    invoicesList.clear();
     try {
       await Singleton.supabaseInstance.client
           .from(RoloxKey.supaBaseUserToInvoiceTable)
@@ -36,14 +30,22 @@ class InvoiceController extends GetxController with StateMixin {
     )
   ''').then((value) {
         value.forEach((element) {
-          projectInvoicesList.add(ProjectModel(
-              amount: element['invoice']['invoiceValueWithoutGst'].toString(),
-              projectName: element['invoice']['invoiceName'],
-              date: element['invoice']['InvoiceDueDate'],
-              noOfInvoice: 1));
+          debugPrint('element--$element');
+          /*     {userid: 206b8fc1-1c22-4f95-885d-3acbd964dc10,
+          invoice: {id: 16, created_at: 2023-07-21T08:19:13.375768+00:00,
+          invoiceName: Invoice for Photo work, invoiceNumber: 123456,
+          invoiceValueWithoutGst: 1234.56, InvoiceDueDate: 21/07/2023,
+
+          hsnCode: 123456, gstCharges: 0, projectId: 3, paid: false}}*/
+
+          invoicesList.add(Invoice(
+              invoiceAmount: element['invoice']['invoiceValueWithoutGst'],
+              invoiceNumber: element['invoice']['invoiceNumber'],
+              invoiceName: element['invoice']['invoiceName'],
+              dueDate: element['invoice']['InvoiceDueDate']));
         });
       });
-      change(projectInvoicesList, status: RxStatus.success());
+      change(invoicesList, status: RxStatus.success());
     } catch (e) {
       e.printError();
       return [];
